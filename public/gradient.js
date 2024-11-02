8c32f14b8cf2337dfaba9c93c8279881235517ab
Matheus Carvalho <matheusfelipe_contato@hotmail.com>
2024-11-02 18:12:02 -0300

    chore: progresso

--- public/gradient.js
+++
@@ -1,5 +0,0 @@
-const root = document.querySelector(":root");
-root.style.setProperty("--gradient-y", `${window.scrollY + 150}px`);
-document.addEventListener("scroll", function (e) {
-  root.style.setProperty("--gradient-y", `${window.scrollY + 150}px`);
-});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -19,10 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${inter.variable} antialiased`}>
-        <Script src="/gradient.js" strategy="lazyOnload" />
-        {children}
-      </body>
+      <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
 }
